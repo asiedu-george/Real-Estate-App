34c5a6a27dd30857327127b5fd41ec30d90d7733
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-03 20:00:08 +0000

    app routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'auth',
-        loadChildren: () => import('../app/auth/auth.module').then(am => am.AuthModule)
+        loadChildren: () => import('../app/auth/auth.module')
+        .then(am => am.AuthModule)
+    },
+    {
+        path: '',
+        loadChildren: () => import('../app/landing-page/landing-page.module')
+        .then(lpm => lpm.LandingPageModule)
     }
 ]
