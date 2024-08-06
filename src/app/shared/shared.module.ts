475c1c2a749a66e18b8678997fd44f3dad0769f5
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-06 14:43:59 +0000

    shared module created

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { CarouselComponent } from './carousel/carousel.component';
+
+
+
+@NgModule({
+  declarations: [
+    SpinnerComponent,
+    CarouselComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [CarouselComponent, SpinnerComponent]
+})
+export class SharedModule { }
