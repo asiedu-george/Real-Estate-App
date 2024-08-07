4730b1f8bb50faa1a5a425c470e2e6447918390e
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-07 09:43:23 +0000

    listing card component added to shared module exports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { ListingCardComponent } from './listing-card/listing-card.component';
 
 
 
 @NgModule({
   declarations: [
     SpinnerComponent,
-    CarouselComponent
+    CarouselComponent,
+    ListingCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CurrencyPipe
   ],
-  exports: [CarouselComponent, SpinnerComponent]
+  exports: [
+    CarouselComponent, 
+    SpinnerComponent, 
+    ListingCardComponent
+  ]
 })
 export class SharedModule { }
