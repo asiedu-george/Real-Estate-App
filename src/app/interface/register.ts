c7ddc393f563b07f33fa9cbfd7f5cc14e61d99ff
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-08 17:39:48 +0000

    register interface refactored

--- src/app/interface/register.ts
+++ src/app/interface/register.ts
@@ -1,7 +1,6 @@
 export interface Register {
     additional_properties: {
-        address: {city: string},
-        profile_picture: string
+        address: {city: string}
     },
     email: string,
     first_name: string,
