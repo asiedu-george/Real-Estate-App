bb0f91fd826c3543857a6351f20015ed23788de3
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-08 17:54:02 +0000

    home listing route defined

---
+++ src/app/home-listing/home-listing-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeListingsComponent } from './home-listings/home-listings.component';
+
+const routes: Routes = [
+  { 
+    path: 'properties',
+    component: HomeListingsComponent,
+    title: 'Just Home | Home Listings Page'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeListingRoutingModule { }
