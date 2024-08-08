9dd533788246b20987079197e97c26499888de98
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-08 17:48:25 +0000

    home listing component created

---
+++ src/app/home-listing/home-listings/home-listings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-listings',
+  templateUrl: './home-listings.component.html',
+  styleUrl: './home-listings.component.scss'
+})
+export class HomeListingsComponent {
+
+}
