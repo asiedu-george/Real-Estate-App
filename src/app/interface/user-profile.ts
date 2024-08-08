58ae526485e481d10fc20a3e89f99421905adbff
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-08 17:39:12 +0000

    user profile interface defined

---
+++ src/app/interface/user-profile.ts
@@ -0,0 +1,9 @@
+export interface UserProfile {
+    additional_properties: {
+        address: {city: string}
+    },
+    email: string,
+    first_name: string,
+    last_name: string,
+    id: string
+}
