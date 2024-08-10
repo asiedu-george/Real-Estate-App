4103d0d0ed8c5a046d956bbd98d60447b134ea5d
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-10 23:47:55 +0000

    rapi api headers added

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,5 @@
 export const constants = {
-    apnKey: '16a91717e2580a2c74093e93d3f6abe8'
+    apnKey: '16a91717e2580a2c74093e93d3f6abe8',
+    rapidApiKey: 'c295efc728msh5eb912dc524a64cp1f169ajsnbf1e4b674ee4',
+    rapidApiHost: 'realty-in-us.p.rapidapi.com'
 }
