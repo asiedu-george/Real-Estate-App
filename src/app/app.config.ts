c239396c39c247330451ce75a3fe8ffdaf4cbbbc
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-10 23:47:45 +0000

    rapid api interceptor added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -8,13 +7,16 @@ import { provideSpinnerConfig } from 'ngx-spinner'
 import { NgToastModule } from 'ng-angular-popup';
 import { authInterceptor } from './interceptors/token/token.interceptor';
 import { headersInterceptor } from './interceptors/headers/headers.interceptor';
+import { rapidApiInterceptor } from './interceptors/rapid-api/rapid-api.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes, withPreloading(PreloadAllModules)), 
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([headersInterceptor,authInterceptor])),
+    provideHttpClient(withInterceptors([
+      headersInterceptor,authInterceptor, rapidApiInterceptor
+    ])),
     provideSpinnerConfig({ type: 'ball-scale-multiple' }),
     importProvidersFrom(NgToastModule)
   ]
