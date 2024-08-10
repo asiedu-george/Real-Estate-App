04ef2220bbcc35bb70cd839b87fc69f5f0c92727
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-10 23:49:24 +0000

    dropdown logic implemented

--- src/app/home-listing/home-listings/home-listings.component.ts
+++ src/app/home-listing/home-listings/home-listings.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './home-listings.component.scss'
 })
 export class HomeListingsComponent {
+  public dropdowns: { [key in 'status' | 'price' | 'bedBath' | 'homeType']: boolean } = {
+    status: false,
+    price: false,
+    bedBath: false,
+    homeType: false
+  };
 
+  toggleDropdown(dropdown: keyof typeof this.dropdowns): void {
+    Object.keys(this.dropdowns).forEach(key => {
+      this.dropdowns[key as keyof typeof this.dropdowns] = (key === dropdown) 
+      ? !this.dropdowns[key as keyof typeof this.dropdowns] : false;
+    });
+  }
 }
