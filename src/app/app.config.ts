9b8d3a84b3ad19a821f820ae367bf37d9407d2b5
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-11 15:50:58 +0000

    provide store and provide effect added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom, isDevMode } from '@angular/core';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -8,16 +8,24 @@ import { NgToastModule } from 'ng-angular-popup';
 import { authInterceptor } from './interceptors/token/token.interceptor';
 import { headersInterceptor } from './interceptors/headers/headers.interceptor';
 import { rapidApiInterceptor } from './interceptors/rapid-api/rapid-api.interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { listingReducers } from './home-listing/store/reducers';
+import { ListingEffects } from './home-listing/store/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes, withPreloading(PreloadAllModules)), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([
-      headersInterceptor,authInterceptor, rapidApiInterceptor
+        headersInterceptor, authInterceptor, rapidApiInterceptor
     ])),
     provideSpinnerConfig({ type: 'ball-scale-multiple' }),
-    importProvidersFrom(NgToastModule)
-  ]
+    importProvidersFrom(NgToastModule),
+    provideStore({'listings': listingReducers}),
+    provideEffects([ListingEffects]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 }
