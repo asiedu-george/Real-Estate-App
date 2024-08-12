88ab0908bce50c684f3d7996fed3808fe998f0ad
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-12 16:59:50 +0000

    rapid api key and host changed

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 export const constants = {
     apnKey: '16a91717e2580a2c74093e93d3f6abe8',
-    rapidApiKey: 'c295efc728msh5eb912dc524a64cp1f169ajsnbf1e4b674ee4',
+    rapidApiKey: '938e703868msh649baf0bf193af8p175b3djsnea2f7144e4a7',
     rapidApiHost: 'realty-in-us.p.rapidapi.com'
 }
