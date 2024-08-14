2626addb5ab3e596d7e066631cd702b82ac00fe5
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-14 08:41:45 +0000

    store states defined for listing details

--- src/app/home-listing/store/state/state.ts
+++ src/app/home-listing/store/state/state.ts
@@ -1,7 +1,14 @@
 import { HomeData } from "../../../interface/home-listing";
+import { DescriptionData } from "../../../interface/listing-description";
 
 export interface HomeListingState {
     isLoading: boolean;
     list: HomeData | null;
-    error: string | null
+    error: string | null;
+}
+
+export interface HomeListingDetailsState {
+    isLoading: boolean;
+    details: DescriptionData | null;
+    error: string | null;
 }
