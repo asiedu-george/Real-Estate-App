b255c9f89d6e795f3325eef259f283377179ce3a
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-14 08:30:36 +0000

    listing details component added to feature route module

--- src/app/home-listing/home-listing-routing.module.ts
+++ src/app/home-listing/home-listing-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeListingsComponent } from './home-listings/home-listings.component';
+import { ListingDetailsComponent } from './listing-details/listing-details.component';
 
 const routes: Routes = [
   { 
     path: '',
     component: HomeListingsComponent,
     title: 'Just Home | Home Listings Page'
+  },
+  {
+    path: ':property_id',
+    component: ListingDetailsComponent,
+    title: 'Just Home | Home Listings Details Page'
   }
 ];
 
