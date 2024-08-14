4f693cb7c85b7a6ee7d590d1d54acc6df4747742
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-14 15:55:02 +0000

    forms module imported

--- src/app/home-listing/home-listing.module.ts
+++ src/app/home-listing/home-listing.module.ts
@@ -4,19 +4,19 @@ import { AsyncPipe, CommonModule, NgOptimizedImage } from '@angular/common';
 import { HomeListingRoutingModule } from './home-listing-routing.module';
 import { HomeListingsComponent } from './home-listings/home-listings.component';
 import {SharedModule} from "../shared/shared.module";
-import { PropertiesComponent } from './properties/properties.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LandingPageModule } from '../landing-page/landing-page.module';
 import { ListingDetailsComponent } from './listing-details/listing-details.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     HomeListingsComponent,
-    PropertiesComponent,
     ListingDetailsComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     HomeListingRoutingModule,
     SharedModule,
     NgOptimizedImage,
