5139c194e67f8462771dd53d7986a86305f4f4d7
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-15 14:24:16 +0000

    listing filters interface defined

--- src/app/interface/listing-filters.ts
+++ src/app/interface/listing-filters.ts
@@ -1,10 +1,26 @@
 export interface ListingFilters {
+    postal_code: string;
     status: string[];
-    minPrice?: number | null;
-    maxPrice?: number | null;
-    minBeds?: number | null;
-    maxBeds?: number | null;
-    minBaths?: number | null;
-    maxBaths?: number | null;
-    homeTypes?: string[];
+    list_price?: Range;
+    type?: string[];
+}
+
+export interface Range {
+    min: number, 
+    max?: number
+}
+
+export enum Status {
+    SOLD = 'sold',
+    FOR_RENT = 'for_rent',
+    FOR_SALE = 'for_sale'
+}
+  
+export enum HomeType {
+    APARTMENT = 'apartment',
+    CONDOS = 'condos',
+    MULTI_FAMILY = 'multi_family',
+    TOWNHOMES = 'townhomes',
+    LAND = 'land',
+    DUPLEX_TRIPLEX = 'duplex_triplex'
 }
