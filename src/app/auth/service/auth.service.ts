92a403edcc9157d7977e9c86559b4edc17df6ec9
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-19 08:45:32 +0000

    auth service methods modified to use the retry rxjs opeartor

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, map, Observable, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, retry, take, tap, throwError } from 'rxjs';
 import { authEnv } from '../../../environments/environment.development';
 import { Login, LoginResponse } from '../../interface/login';
 import { Register, RegisterResponse } from '../../interface/register';
@@ -8,93 +8,58 @@ import { UpdatePassword, UpdatePasswordResponse } from '../../interface/update-p
 import { UserProfile } from '../../interface/user-profile';
 import { Router } from '@angular/router';
 import { RefreshTokenResponse } from '../../interface/refresh-token-response';
+import { Store } from '@ngrx/store';
+import { selectRefreshToken } from '../store/login.selectors';
+import { NgToastService } from 'ng-angular-popup';
+import { constants } from '../../constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   protected authUrl: string = authEnv.baseUrl;
-  private activeUserSubject: BehaviorSubject<LoginResponse | null>;
-  public activeUser: Observable<LoginResponse | null>;
+  private readonly token = this.store.selectSignal(selectRefreshToken)
 
-  constructor(private http: HttpClient, private router: Router) {
-    const storedUser: string | null = localStorage.getItem('activeUser');
-    if (storedUser) {
-      const parsedUser = JSON.parse(storedUser);
-      this.activeUserSubject = new BehaviorSubject<LoginResponse | null>(parsedUser);
-    } else {
-      this.activeUserSubject = new BehaviorSubject<LoginResponse | null>(null);
-    }
-    this.activeUser = this.activeUserSubject.asObservable();
-  }
-  
-  public storedUserData(response: LoginResponse): void {
-    localStorage.setItem('activeUser', JSON.stringify(response));
-    this.activeUserSubject.next(response);
-  }
-
-  get activeUserValue(): LoginResponse | null {
-    return this.activeUserSubject.value;
-  }  
-
-  refreshStoredUser(): void {
-    const storedUser: string | null = localStorage.getItem('activeUser');
-    if (storedUser) {
-      const parsedUser = JSON.parse(storedUser);
-      this.activeUserSubject.next(parsedUser);
-    } else {
-      this.activeUserSubject.next(null);
-    }
-  }  
+  constructor(
+    private http: HttpClient, 
+    private store: Store,
+    private router: Router,
+    private toast: NgToastService
+  ) {}
 
   register(user: Register): Observable<RegisterResponse> {
-    return this.http.post<RegisterResponse>(`${this.authUrl}user/signup`, user);
+    return this.http.post<RegisterResponse>(`${this.authUrl}user/signup`, user)
+    .pipe(take(1));
   }
 
   login(user: Login): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>(`${this.authUrl}user/login`, user).pipe(
-      tap((response: LoginResponse) => {
-        this.storedUserData(response);
-        this.router.navigateByUrl('/listings');
-      }),
-      catchError(err => {
-        return throwError(() => err);
-      })
-    );
+    return this.http.post<LoginResponse>(`${this.authUrl}user/login`, user)
+    .pipe(take(1))
   }
 
   updatePassword(user: UpdatePassword): Observable<UpdatePasswordResponse> {
-    return this.http.post<UpdatePasswordResponse>(`${this.authUrl}user/update-password`, user);
+    return this.http.post<UpdatePasswordResponse>(`${this.authUrl}user/update-password`, user)
+    .pipe(take(1));
   }
 
   validateUser(): Observable<{ message: string }> {
-    return this.http.get<{ message: string }>(`${this.authUrl}user/validate`);
+    return this.http.get<{ message: string }>(`${this.authUrl}user/validate`)
+    .pipe(retry(2));
   }
 
   getUserProfile(): Observable<UserProfile> {
-    return this.http.get<UserProfile>(`${this.authUrl}/user/profile`);
+    return this.http.get<UserProfile>(`${this.authUrl}user/profile`)
+    .pipe(retry(2));
+  }
+
+  refreshToken() {
+    return this.http.post<RefreshTokenResponse>(
+      `${this.authUrl}user/refresh-token`,
+      { refresh_token: this.token() }
+    ).pipe(retry(2))
   }
 
   logout(): void {
-    localStorage.removeItem('activeUser');
-    this.activeUserSubject.next(null);
-    this.router.navigateByUrl('/auth/login');
-  }
-
-  refreshToken(refresh_token: string): Observable<RefreshTokenResponse> {
-    const body = JSON.stringify({ refresh_token: refresh_token });
-
-    return this.http.post<RefreshTokenResponse>(`${this.authUrl}/user/refresh-token`, body)
-    .pipe(
-      map(response => response),
-      catchError(err => {
-        this.logout();
-        return throwError(() => err);
-      })
-    );
-  }
-
-  isAuthenticated(): boolean {
-    return !!this.activeUserValue?.login_token;
+    this.router.navigateByUrl('/auth/login')
   }
 }
