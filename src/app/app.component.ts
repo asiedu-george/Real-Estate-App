731f96e1e279fc65405346f71f4d61c3e43bb0a3
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-19 08:42:55 +0000

    login state persisted in the application
    The code ensures that the authentication state is preserved when the user leaves the application (closes the tab or browser).
    When the user returns, the saved state is restored from local storage, allowing the application to pick up where it left off. After restoring the state, the local storage is cleared to prevent future conflicts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { NgToastModule } from 'ng-angular-popup';
+import { selectLoginFeature } from './auth/store/login.selectors';
+import { getStoreData } from './app-store/app.actions';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,22 @@ import { NgToastModule } from 'ng-angular-popup';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'just-home';
+export class AppComponent implements OnInit {
+  private store = inject(Store);
+  private state = this.store.selectSignal(selectLoginFeature);
+
+  @HostListener('window:beforeunload', ['$event'])
+  beforeunloadHandler(): void {
+    localStorage.setItem('authState', JSON.stringify(this.state()));
+  } 
+  
+  ngOnInit(): void {
+    const retrievedState = localStorage.getItem('authState');
+    if (retrievedState) {
+      const auth = JSON.parse(retrievedState)
+      this.store.dispatch(getStoreData(auth));
+    }
+    localStorage.clear()
+  }
+  
 }
