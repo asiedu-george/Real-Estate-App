e2c76ade8e31f83d1a7ff521b98b4858c1fc377d
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-19 17:06:21 +0000

    data-theme styles defined

--- src/app/composable/themeSwitch.ts
+++ src/app/composable/themeSwitch.ts
@@ -6,12 +6,14 @@ export const useThemeSwitcher = (key: string, initialValue: string) => {
     const storedValue: string | null = localStorage.getItem(key)
     if(storedValue !== null) {
         value.set(storedValue)
+        document.body.setAttribute('data-theme', storedValue);
     }
 
     function storageHandler(e: StorageEvent): void {
         if(e.key === key) {
             const newValue: string = e.newValue !== null ? e.newValue : ''
             value.set(newValue)
+            document.body.setAttribute('data-theme', newValue);
         }
     }
 
