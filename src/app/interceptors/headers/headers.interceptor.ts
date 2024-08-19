4906ac3dfd00f38a19cba2ff81e2d405455c37a9
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-19 08:55:35 +0000

    This setup ensures that the user is validated before any request to the RapidAPI is mad
    This setup ensures that the user is validated before any request to the RapidAPI is made, and it prevents unauthorized requests if validation fails.

--- src/app/interceptors/headers/headers.interceptor.ts
+++ src/app/interceptors/headers/headers.interceptor.ts
@@ -1,11 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { constants } from '../../constants';
+import { authEnv } from '../../../environments/environment.development';
 
 export const headersInterceptor: HttpInterceptorFn = (req, next) => {
-  const modifiedRequest = req.clone({
-    setHeaders: {
-      'X-APN': constants.apnKey
-    }
-  })
-  return next(modifiedRequest);
+  const authApi = authEnv.baseUrl
+  if(req.url.startsWith(authApi)) {
+    const modifiedRequest = req.clone({
+      setHeaders: {
+        'X-APN': constants.apnKey
+      }
+    })
+    return next(modifiedRequest);
+  }
+  return next(req)
 };
