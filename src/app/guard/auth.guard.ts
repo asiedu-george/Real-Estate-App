fad060c1fbbd2252e18579c3dc72a1ba27463869
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-19 08:47:14 +0000

    select login selector used to get login token to implement auth guard

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,38 +1,45 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, CanMatchFn } from '@angular/router';
-import { AuthService } from '../auth/service/auth.service';
+import { CanActivateFn } from '@angular/router';
 import { Router } from '@angular/router';
 import { NgToastService } from 'ng-angular-popup';
+import { Store } from '@ngrx/store';
+import { LoginSate } from '../home-listing/store/state/state';
+import { selectLoginToken } from '../auth/store/login.selectors';
+import { map, take } from 'rxjs';
+import { constants } from '../constants';
 
 const getDependencies = () => {
     return {
-        authService: inject(AuthService),
         router: inject(Router),
-        toast: inject(NgToastService)
+        toast: inject(NgToastService),
+        store: inject(Store<LoginSate>)
     }
 }
 
 const handleAuthorizedAccess = (router: Router, toast: NgToastService): void =>{
-    toast.warning('Unauthorized route access, please login', 'Access denied', 5000)
+    toast.warning(
+        constants.unauthorizedAccess, 
+        constants.accessDenied, 
+        constants.spinnerDuration
+    )
     router.navigateByUrl('/auth/login')
 }
 
-const checkAuth = (): boolean => {
-    const { authService, router, toast } = getDependencies()
-    const authenticatedUser = authService.isAuthenticated()
-
-    if(authenticatedUser) {
-        return true
-    } else {
-        handleAuthorizedAccess(router, toast)
-        return false
-    }
+const checkAuth = () => {
+    const { router, toast, store } = getDependencies()
+    return store.select(selectLoginToken).pipe(
+        take(1),
+        map((token) => {
+            if(token) {
+                return true
+            } else {
+                handleAuthorizedAccess(router, toast)
+                return false
+            }
+        })
+    )
 }
 
 export const canActivate: CanActivateFn = () => {
-    return checkAuth()
-}
-
-export const canMatch: CanMatchFn = () => {
     return checkAuth()
 }
