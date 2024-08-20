801b07b7737d4e51179e5c9e0e5c080e0e223666
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-20 16:18:37 +0000

    update password method removed as its unused

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,10 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, retry, take } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { authEnv } from '../../../environments/environment.development';
 import { Login, LoginResponse } from '../../interface/login';
 import { Register, RegisterResponse } from '../../interface/register';
-import { UpdatePassword, UpdatePasswordResponse } from '../../interface/update-password';
 import { UserProfile } from '../../interface/user-profile';
 import { Router } from '@angular/router';
 import { RefreshTokenResponse } from '../../interface/refresh-token-response';
@@ -33,11 +32,6 @@ export class AuthService {
   login(user: Login): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${this.authUrl}user/login`, user)
     .pipe(take(1))
-  }
-
-  updatePassword(user: UpdatePassword): Observable<UpdatePasswordResponse> {
-    return this.http.post<UpdatePasswordResponse>(`${this.authUrl}user/update-password`, user)
-    .pipe(take(1));
   }
 
   validateUser(): Observable<{ message: string }> {
