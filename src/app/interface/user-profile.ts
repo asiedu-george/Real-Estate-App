364467cc68239a03ef9af2dc44e279f9b4d6ccb2
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-20 16:24:38 +0000

    unused properties removed

--- src/app/interface/user-profile.ts
+++ src/app/interface/user-profile.ts
@@ -1,18 +1,6 @@
 export interface UserProfile {
-    additional_properties: {
-        address: {city: string}
-    },
     email: string,
     first_name: string,
     last_name: string,
     id: string
 }
-
-export interface UserDetails {
-    email: string;
-    role: string;
-    firstname: string;
-    lastname: string;
-    client: string;
-    exp: number;
-  }
