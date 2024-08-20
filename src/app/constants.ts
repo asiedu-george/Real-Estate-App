8cc4b62c3cea68bc3cdc82a61f277f0467893d75
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-20 15:02:40 +0000

    deleted

--- src/app/constants.ts
+++
@@ -1,5 +0,0 @@
-export const constants = {
-    apnKey: '16a91717e2580a2c74093e93d3f6abe8',
-    rapidApiKey: '6153baa1dbmshd7118b4afba0465p1918edjsnbfb459232043',
-    rapidApiHost: 'realty-in-us.p.rapidapi.com'
-}
