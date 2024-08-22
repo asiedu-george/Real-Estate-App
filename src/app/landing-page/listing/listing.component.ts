219c338f3114d879f67b77f5fc9b7ff589d059ed
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-22 13:49:08 +0000

    actions codes removed

--- src/app/landing-page/listing/listing.component.ts
+++ src/app/landing-page/listing/listing.component.ts
@@ -1,28 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { selectListings } from '../../home-listing/store/selectors/selectors';
-import { HomeListingState } from '../../home-listing/store/state/state';
-import { getHomeListing } from '../../home-listing/store/actions/actions';
-import { constants } from '../../../environments/constants';
-import { ListingFilters } from '../../interface/listing-filters';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
   styleUrl: './listing.component.scss'
 })
-export class ListingComponent implements OnInit {
-  public listings$ = this.store.select(selectListings)
-  public filters: ListingFilters = {
-    postal_code: '',
-    status: constants.status
-  }
-
-  constructor(private store: Store<HomeListingState>) {}
-
-  ngOnInit(): void {
-    this.store.dispatch(getHomeListing({ 
-      filters: this.filters
-    }))
-  }
-}
+export class ListingComponent {}
