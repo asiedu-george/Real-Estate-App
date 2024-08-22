fbca77eb41761f7f30a595d029fe492fdad4bbbf
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-08-22 16:18:44 +0000

    updated

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+        production: false,
+        rapidApiUrl: 'https://realty-in-us.p.rapidapi.com/',
+        authUrl: 'https://go-auth-v1.onrender.com/',
+        apnKey: '16a91717e2580a2c74093e93d3f6abe8',
+        rapidApiKey: '92694f6f07mshe87ecabc82ac630p188b7djsnbce56d2b630d',
+        rapidApiHost: 'realty-in-us.p.rapidapi.com',
+    }
